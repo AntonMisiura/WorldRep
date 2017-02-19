6a41db3a7eb71c552b0555c24a8b66bc12eaa964
antonmisiura <misiura.anton@gmail.com>
2017-02-19 22:05:06 +0200

    InitialCommit

---
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using TheWorld.ViewModels;
+using TheWorld.Services;
+using Microsoft.Extensions.Configuration;
+using TheWorld.Models;
+using Microsoft.Extensions.Logging;
+
+namespace TheWorld.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        private ILogger<AppController> _logger;
+        private IWorldRepository _repository;
+        private IConfigurationRoot _config;
+        private IMailService _mailService;
+
+        public AppController(IMailService mailService, IConfigurationRoot config,
+            IWorldRepository repository,
+            ILogger<AppController> logger)
+        {
+            _mailService = mailService;
+            _config = config;
+            _repository = repository;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            try
+            {
+                var data = _repository.GetAllTrips();
+
+                return View(data);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"Failed to get trips in Index page: {ex.Message}");
+                return Redirect("/error");
+            }
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact (ContactViewModel model)
+        {
+            if (model.Email.Contains("aol.com")) ModelState.
+                    AddModelError("", "We don't support AOL addresses!");
+
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "From TheWorld", model.Message);
+
+                ModelState.Clear();
+
+                ViewBag.UserMessage = "Message Sent!";
+            }
+
+            return View();
+        }
+    }
+}
---
+++ src/TheWorld/Models/WorldRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TheWorld.Models
+{
+    public class WorldRepository : IWorldRepository
+    {
+        private ILogger<WorldRepository> _logger;
+        private WorldContext _context;
+
+        public WorldRepository(WorldContext context, ILogger<WorldRepository> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public IEnumerable<Trip> GetAllTrips()
+        {
+            _logger.LogInformation("Getting All Trips from the Database");
+
+            return _context.Trips.ToList();
+        }
+
+        public Trip GetTripByName(string tripName)
+        {
+            return _context.Trips
+                .Include(t => t.Stops)
+                .Where(t => t.Name == tripName)
+                .FirstOrDefault();
+        }
+
+        public void AddTrip(Trip trip)
+        {
+            _context.Add(trip);
+        }
+
+        public void AddStop(string tripName, Stop newStopp)
+        {
+            var trip = GetTripByName(tripName);
+
+            if(trip != null)
+            {
+                trip.Stops.Add(newStopp);
+                _context.Stops.Add(newStopp);
+            }
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return (await _context.SaveChangesAsync()) > 0;
+        }
+    }
+}
